de9ec5f193577f9150c8a6e02b07061b9d78232b
rysz-eriksson <ryszard.wolosiuk@gmail.com>
2020-08-12 16:37:31 +0200

    docs: fix webpack issue

--- src/components/lecturesItemVod.js
+++ src/components/lecturesItemVod.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import database from '../firebase/firebase';
 import { handleError } from './submitForm';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    padding: 5px;
+    background-color: white;
+    cursor: pointer;
+    &:focus {
+        outline-color: #f18700;
+    }
+`
 
 export default ({ lecture, index }) => {
 
@@ -17,7 +27,7 @@ export default ({ lecture, index }) => {
             <td>{lecture.stream_title}</td>
             <td>{lecture.lecturer_name}</td>
             <td>{lecture.image_url}</td>
-            <td><button className="delete" onClick={onDeleteAction}>Delete</button></td>
+            <td><Button onClick={onDeleteAction}>Delete</Button></td>
         </tr>
 
     )
