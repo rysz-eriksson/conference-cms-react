266d89e4e75c59c09cf236aad7abf94d06fbb2cd
rysz-eriksson <ryszard.wolosiuk@gmail.com>
2020-08-12 12:23:52 +0200

    refactor: lists components with hooks

--- src/components/lecturesListLive.js
+++ src/components/lecturesListLive.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import database from '../firebase/firebase';
 import LectureItemLive from './lecturesItemLive';
 
-export default class LecturesListLive extends React.Component {
-    state = {
-        liveMovies: []
-    }
-    componentDidMount() {
+export default () => {
+
+    const [liveMovies, setMovies] = useState([])
+
+    useEffect(() => {
         database.ref('live-library').on('value', (snapshot) => {
             let lectures = []
             snapshot.forEach((childSnap) => {
@@ -14,30 +14,68 @@ export default class LecturesListLive extends React.Component {
                     id: childSnap.key,
                     ...childSnap.val()
                 })
-                this.setState({liveMovies: [...lectures]})
+                setMovies([...lectures])
         })
     }, (err) => {
         alert(err)
         })
-    }
-    render() {
-        const { liveMovies } = this.state;
-        return (
-            <table>
-            <thead>
-              <tr>
-                <th>track_id</th>
-                <th>stream_id</th>
-                <th>title</th>
-                <th>lecturer_name</th>
-              </tr>
-            </thead>
-            <tbody>
-            {liveMovies.map((item) => (
-                <LectureItemLive key={item.id} lecture={item} />
-            ))}
-            </tbody>
-          </table>
-        )
-    }
+    }, [])
+
+    return (
+        <table>
+        <thead>
+          <tr>
+            <th>track_id</th>
+            <th>stream_id</th>
+            <th>title</th>
+            <th>lecturer_name</th>
+          </tr>
+        </thead>
+        <tbody>
+        {liveMovies.map((item) => (
+            <LectureItemLive key={item.id} lecture={item} />
+        ))}
+        </tbody>
+      </table>
+    )
 }
+
+// export default class LecturesListLive extends React.Component {
+//     state = {
+//         liveMovies: []
+//     }
+//     componentDidMount() {
+//         database.ref('live-library').on('value', (snapshot) => {
+//             let lectures = []
+//             snapshot.forEach((childSnap) => {
+//                 lectures.push({
+//                     id: childSnap.key,
+//                     ...childSnap.val()
+//                 })
+//                 this.setState({liveMovies: [...lectures]})
+//         })
+//     }, (err) => {
+//         alert(err)
+//         })
+//     }
+//     render() {
+//         const { liveMovies } = this.state;
+//         return (
+//             <table>
+//             <thead>
+//               <tr>
+//                 <th>track_id</th>
+//                 <th>stream_id</th>
+//                 <th>title</th>
+//                 <th>lecturer_name</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//             {liveMovies.map((item) => (
+//                 <LectureItemLive key={item.id} lecture={item} />
+//             ))}
+//             </tbody>
+//           </table>
+//         )
+//     }
+// }
--- src/components/lecturesListVod.js
+++ src/components/lecturesListVod.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import database from '../firebase/firebase';
 import LectureItemVod from './lecturesItemVod';
 
-export default class LecturesListVod extends React.Component {
-    state = {
-        vodMovies: []
-    }
-    componentDidMount() {
+export default () => {
+
+   const [vodMovies, setMovies] = useState([])
+
+    useEffect(() => {
         database.ref('vod-library').on('value', (snapshot) => {
             let lectures = []
             if (snapshot.val() !== null) {
@@ -17,31 +17,29 @@ export default class LecturesListVod extends React.Component {
                     })
                 })
             }
-            this.setState({vodMovies: [...lectures]})
+            setMovies([...lectures])
     }, (err) => {
         alert(err)
         })
-}
-    render() {
-        const { vodMovies } = this.state;
-        return (
-            <table>
-      <thead>
-        <tr>
-          <th>Lp.</th>
-          <th>stream_id</th>
-          <th>title</th>
-          <th>lecturer_name</th>
-          <th>image_url</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {vodMovies.map((item, index) => (
-        <LectureItemVod key={item.id} lecture={item} index={index}/>
-        ))}
-      </tbody>
-    </table>
-        )
-    }
+    }, [])
+
+    return (
+        <table>
+  <thead>
+    <tr>
+      <th>Lp.</th>
+      <th>stream_id</th>
+      <th>title</th>
+      <th>lecturer_name</th>
+      <th>image_url</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    {vodMovies.map((item, index) => (
+    <LectureItemVod key={item.id} lecture={item} index={index}/>
+    ))}
+  </tbody>
+</table>
+    )
 }
